52258e31ca7554c2417b889c98958ed1c94e51c9
zxcmhy <3405627313@qq.com>
2026-09-27 01:24:15 +0800

    stage1: 完成 Tensor 基础练习,优化形状打印在循环之前

--- stage1/my_tensor_practice.py
+++ stage1/my_tensor_practice.py
@@ -19,7 +19,7 @@ y = x**2+2*x
 y.backward()
 print("dy/dx:", x.grad)
 
-#3.手动线性回归（该小学习率）
+#3.手动线性回归（改小学习率）
 x_data = torch.tensor([[1.0],[2.0],[3.0],[4.0],[5.0]])
 y_data = torch.tensor([[5.0],[8.0],[11.0],[14.0],[17.0]])
 
@@ -27,10 +27,12 @@ w = torch.tensor([[0.0]], requires_grad=True)
 b = torch.tensor([[0.0]], requires_grad=True)
 lr = 0.005
 
+# 先确认形状
+y_pred_check = x_data @ w + b
+print("y_pred.shape:", y_pred_chek.shape)
+
 for epoch in range(5000):
 	y_pred = x_data @ w + b
-	if epoch == 0:
-		print("y_pred.shape:", y_pred.shape)
 	loss = ((y_pred - y_data)**2).mean()
 	loss.backward()
 	with torch.no_grad():
